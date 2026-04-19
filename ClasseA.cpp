cfc13601e655f50f53a9f140ee0b1805a0e5e546
Tainara Souza <tainarafms7@gmail.com>
2026-04-19 14:24:56 -0300

    Adicionando ClasseA e método getSoma

---
+++ ClasseA.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class ClasseA {
+public:
+    // Método que retorna a soma de dois inteiros
+    int getSoma(int a, int b) {
+        return a + b;
+    }
+};
